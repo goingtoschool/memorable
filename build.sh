a87477c00ca64df2dba7680daf7e56be57b4b429
Going to School <56346761+goingtoschool@users.noreply.github.com>
2019-10-10 13:18:25 +0530

    committed.
    committed by - Prabhat Kumar.

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# ========================++== memorable® ==++=======================
+#
+#                                 GTS
+#                            Going to School
+#                       https://goingtoschool.com/
+#
+#        Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+#     Copyright © 2019 - 2020, Prabhat Kumar. All rights reserved.
+# Copyright © 2019 - 2020, Sequømics Corporation, All rights reserved.
+# ====================================================================

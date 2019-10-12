290e1b3a5e69a5c73e736637397da3f8c5d057d1
Going to School <56346761+goingtoschool@users.noreply.github.com>
2019-10-12 16:30:36 +0530

    committed.
    grunt-contrib-copy — Copy files and directory(s).
    Usage: $ npm install grunt-contrib-copy --save-dev | Help: https://github.com/gruntjs/grunt-contrib-copy

---
+++ tasks/options/copy.js
@@ -0,0 +1,36 @@
+/*!
+ * ----------
+ * memorable®
+ * ----------
+ * @NPM — https://www.npmjs.com/~goingtoschool  | https://goingtoschool.com/
+ * ___________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : ømics Incorporated.
+ * @copyright : ømics Incorporated.
+ * ___________________________________________________________________________
+ *
+ * @date      : 10-10-2019
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-cli
+ * @build     : SEED™ — Skövde
+ *              └────── A ømics Product.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "memorable®".
+ * ___________________________________________________________________________
+ */
+
+// global __dirname: true
+// global require: true
+
+// Invoking strict mode.
+"use strict";
